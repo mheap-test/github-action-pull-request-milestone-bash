2e5a5625d149c1e61b4492efc33e9fea2e318de7
Michael Heap <m@michaelheap.com>
2020-08-30 11:07:54 +0100

    Add guard rails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "This is my action, and it's working!"
+if [[ "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
+  echo "This action only runs on pull_request.closed"
+  echo "Found: $GITHUB_EVENT_NAME"
+  exit 1
+fi
+
+ACTION=$(jq -r ".action" $GITHUB_EVENT_PATH)
+if [[ "$ACTION" != "closed" ]]; then
+  echo "This action only runs on pull_request.closed"
+  echo "Found: $GITHUB_EVENT_NAME.$ACTION"
+  exit 1
+fi
+
+IS_MERGED=$(jq -r ".pull_request.merged" $GITHUB_EVENT_PATH)
+if [[ "$IS_MERGED" != "true" ]]; then
+  echo "Pull request closed without merge"
+  exit 0
+fi
